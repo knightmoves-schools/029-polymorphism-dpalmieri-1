aa82e97bf388197481f61845985cf028123d9bce
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-02-18 11:06:20 -0600

    Update Animals.cs

--- Animals.cs
+++ Animals.cs
@@ -2,11 +2,10 @@
 
 public class Trainer{
     public string Listen(Animal[] animals) {
-        string sounds = "";
+        public string sounds = "";
         
-        foreach(Animal animal in animals){
-            public abstract string Say();
-        }
+        public abstract string Say();
+        
 
         return sounds;
     }
@@ -17,22 +16,23 @@ public abstract class Animal {
 }
 
 public class Cat : Animal{
-    public override string Say() : base(Say()){
+    public override string Say(){
         return "meow";
     }
 }
 
 public class Dog : Animal{
-    public override string Say() : base(Say()){
+    public override string Say(){
         return "woof";
     }
 }
 
 public class Bird  : Animal{
-    public override string Say() : base(Say()){
+    public override string Say(){
         return "chirp";
     }
 }
+
 
 
 
