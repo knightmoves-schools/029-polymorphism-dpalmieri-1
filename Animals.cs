ddbf77b87135ff30cc0975e145be20049521c356
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-02-18 11:02:56 -0600

    Update Animals.cs

--- Animals.cs
+++ Animals.cs
@@ -1,11 +1,11 @@
 ﻿namespace knightmoves;
 
-public abstract class Trainer{
+public class Trainer{
     public string Listen(Animal[] animals) {
         string sounds = "";
-
+        
         foreach(Animal animal in animals){
-            public abstract string Say() += sounds;
+            public abstract string Say();
         }
 
         return sounds;
@@ -17,21 +17,22 @@ public abstract class Animal {
 }
 
 public class Cat : Animal{
-    public override string Say() {
+    public override string Say() : base(Say()){
         return "meow";
     }
 }
 
 public class Dog : Animal{
-    public override string Say() {
+    public override string Say() : base(Say()){
         return "woof";
     }
 }
 
 public class Bird  : Animal{
-    public override string Say() {
+    public override string Say() : base(Say()){
         return "chirp";
     }
 }
+
 
 
